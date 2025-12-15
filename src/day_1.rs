168b74f5d09f092e1b41197a34f6e61d045dcada
swapsCAPS <verstegen.daan@gmail.com>
2025-12-15 20:58:08 +0100

    day 1 - part 2

--- src/day_1.rs
+++ src/day_1.rs
@@ -13,6 +13,7 @@ pub fn run() {
     let content = fs::read_to_string("inputs/day-1.txt").expect("Could not read file");
 
     let mut current: i16 = 50;
+    let mut prev_cur: i16 = current;
     let mut password = 0;
 
     for line in content.lines() {
@@ -25,17 +26,28 @@ pub fn run() {
             current = current + num % 100;
         }
 
-        // Cap results
-        if current < 0 {
-            current = 100 + current
-        }
-        if current > 99 {
-            current = current - 100
+        if num >= 100 {
+            password += (num as f32 / 100.0).floor() as i16;
         }
 
         if current == 0 {
             password += 1;
+        } else if current < 0 {
+            current = current + 100;
+            if prev_cur != 0 {
+                password += 1;
+            }
+        } else if current > 99 {
+            current = current - 100;
+            password += 1;
         }
+
+        prev_cur = current;
+
+        println!(
+            "dir: {}, num: {}, cur: {}, pass: {}",
+            dir, num, current, password
+        );
     }
 
     println!("Password: {}", password)
